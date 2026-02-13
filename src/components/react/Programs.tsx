e2443778442c4a8e70f07d3dadc45b02c0ff8b48
ijquenta <ivanjosuequentavargas@gmail.com>
2026-02-13 12:38:14 -0400

    feat: remove historias and impacto pages, add library, present, record, where-we-work, and who-we-are pages
    style: update components styles for programs content
    
    fix: adjust tailwind safelist configuration

--- src/components/react/Programs.tsx
+++ src/components/react/Programs.tsx
@@ -12,10 +12,10 @@ const Programs = ({ children, date }: ProgramsProps) => {
                     <div className="lg:col-10">
                         <div className="row lg:mt-0">
                             <div className="lg:col-3">
-                                <h6 className="mb-4 pl-7 text-lg lg:mt-0 lg:pl-0">{date}</h6>
+                                <h6 className="mb-4 pl-7 text-lg lg:mt-0 lg:pl-0 font-bold text-gray-900 dark:text-white">{date}</h6>
                             </div>
                             <div className="lg:col-9 lg:border-l border-text/10 lg:pb-10 lg:pl-10">
-                                <div className="programs-content p-6 lg:p-10">
+                                <div className="programs-content p-6 lg:p-0 space-y-3">
                                     {children}
                                 </div>
                             </div>
